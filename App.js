8a16e272f6f59c30973dce3145e0e69eec716d02
kurmangaliyev-a <40716296+kurmangaliyev-a@users.noreply.github.com>
2022-01-16 23:15:34 +0600

    scroll todos

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import { Navbar } from "./src/navbar/Navbar";
 import { AddTodo } from "./src/addTodo/AddTodo";
+import { Todo } from "./src/todo/Todo";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (title) => {
+    //const newTodo = {
+    //  id: Date.now().toString(),
+    //  title: title,
+    //};
+    //setTodos(todos.concat([newTodo]))
+    //setTodos((prevTodos) => {
+    //  return [...prevTodos, newTodo];
+    //});
+    setTodos((prev) => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title,
+      },
+    ]);
+  };
+
   return (
     <View>
       <Navbar title="Todo App!"></Navbar>
       <View style={styles.container}>
-        <AddTodo></AddTodo>
+        <AddTodo onSubmit={addTodo}></AddTodo>
+        <FlatList
+          style={styles.todoList}
+          keyExtractor={(item) => item.id.toString()}
+          data={todos}
+          renderItem={({ item }) => <Todo todo={item}></Todo>}
+        ></FlatList>
       </View>
 
       <StatusBar style="auto" />
@@ -19,6 +47,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 30,
-    paddingVertical: 20,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  todoList: {
+    backgroundColor: "#efe",
   },
 });
--- src/addTodo/AddTodo.js
+++ src/addTodo/AddTodo.js
@@ -1,11 +1,36 @@
-import React from "react";
-import { View, StyleSheet, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, TextInput, Button, Alert } from "react-native";
 
-export const AddTodo = (props) => {
+export const AddTodo = ({ onSubmit }) => {
+  const [value, SetValue] = useState("");
+  const pressHandler = () => {
+    if (value.trim()) {
+      onSubmit(value);
+      SetValue("");
+    } else {
+      //error
+      Alert.alert("Ошибка", "Нельзя создать пустое задание", [
+        {
+          text: "отмена",
+          onPress: () => console.log("Отмена"),
+          style: "cancel",
+        },
+        { text: "окей", onPress: () => console.log("OK Pressed") },
+        { text: "не окей1", onPress: () => console.log("не OK Pressed") },
+        { text: "не окей2", onPress: () => console.log("не OK Pressed") },
+      ]);
+    }
+  };
   return (
     <View style={styles.block}>
-      <TextInput style={styles.input}></TextInput>
-      <Button title="Добавить"></Button>
+      <TextInput
+        style={styles.input}
+        onChangeText={SetValue}
+        value={value}
+        placeholder="Введите название задачи"
+        autoCapitalize="sentences"
+      ></TextInput>
+      <Button title="Добавить" onPress={pressHandler}></Button>
     </View>
   );
 };
@@ -15,6 +40,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    marginBottom: 15,
   },
   input: {
     width: "60%",
